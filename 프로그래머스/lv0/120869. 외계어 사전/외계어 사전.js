6d75a68bd69e3fa508891a65d9714444fdd3cd85
Hyuk <ish0411@naver.com>
2023-01-25 13:04:19 +0900

    [level 0] Title: 외계어 사전, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120869. 외계어 사전/외계어 사전.js
@@ -0,0 +1,16 @@
+const solution = (spell, dic) => {
+  for (let i = 0; i < dic.length; i++) {
+    if (dic[i].length === spell.length) {
+      let count = 0;
+      for (let j = 0; j < spell.length; j++) {
+        if (dic[i].includes(spell[j])) {
+          count++;
+        }
+      }
+      if (count === spell.length) {
+        return 1;
+      }
+    }
+  }
+  return 2;
+};
