b3db316f80526b3c7d3de75bb028a21a14abf066
Hyuk <ish0411@naver.com>
2023-08-09 13:15:17 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.js
+++ 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.js
@@ -1,9 +1,3 @@
 function solution(n) {
-  return parseInt(('' + getThird(n)).split('').reverse().join(''), 3);
-}
-
-function getThird(num) {
-  if (num < 3) return num;
-  if (parseInt(num / 3) < 3) return `${parseInt(num / 3)}${num % 3}`;
-  return `${getThird(parseInt(num / 3))}${num % 3}`;
+  return parseInt([...n.toString(3)].reverse().join(''), 3);
 }
