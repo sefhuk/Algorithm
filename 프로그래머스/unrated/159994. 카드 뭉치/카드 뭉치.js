f18486d445edc6a6e052784bee19096045b23be2
Hyuk <ish0411@naver.com>
2023-08-15 23:46:44 +0900

    [unrated] Title: 카드 뭉치, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.js
@@ -0,0 +1,16 @@
+function solution(cards1, cards2, goal) {
+  let answer = 'Yes';
+
+  goal.forEach((e) => {
+    if (cards1[0] === e) {
+      cards1.shift();
+    } else if (cards2[0] === e) {
+      cards2.shift();
+    } else {
+      answer = 'No';
+      return;
+    }
+  });
+
+  return answer;
+}
