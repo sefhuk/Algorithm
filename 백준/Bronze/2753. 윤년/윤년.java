fafcd42b914f0d63377380da9b88155348424494
Hyuk <ish0411@naver.com>
2023-11-14 16:06:57 +0900

    [Bronze V] Title: 윤년, Time: 124 ms, Memory: 14264 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -0,0 +1,18 @@
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int year = Integer.parseInt(br.readLine());
+
+        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+            bw.write("1");
+        } else {
+            bw.write("0");
+        }
+
+        bw.close();
+    }
+}
