953b8650620a598a0c9da71f0176dfea6cf93195
Hyuk <ish0411@naver.com>
2023-11-14 18:15:47 +0900

    [Bronze II] Title: 공 바꾸기, Time: 144 ms, Memory: 15880 KB -BaekjoonHub

---
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[] basket = new int[N];
+        for (int i = 0; i < N; i++) {
+            basket[i] = i + 1;
+        }
+
+        for (int a = 0; a < M; a++) {
+            st = new StringTokenizer(br.readLine());
+            int i = Integer.parseInt(st.nextToken());
+            int j = Integer.parseInt(st.nextToken());
+            swap(basket, i-1, j-1);
+        }
+
+        for (int i : basket) {
+            bw.write(i + " ");
+        }
+
+        bw.close();
+    }
+
+    public static void swap(int[] arr, int a, int b) {
+        int tmp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = tmp;
+    }
+}
