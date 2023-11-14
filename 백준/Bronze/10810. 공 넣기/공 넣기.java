8d1a15a1c0cb5f557abd52aeccd254453366f107
Hyuk <ish0411@naver.com>
2023-11-14 18:07:23 +0900

    [Bronze III] Title: 공 넣기, Time: 148 ms, Memory: 15872 KB -BaekjoonHub

---
+++ 백준/Bronze/10810. 공 넣기/공 넣기.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[] basket = new int[N];
+        Arrays.fill(basket,0);
+
+        for (int a = 0; a < M; a++) {
+            st = new StringTokenizer(br.readLine());
+            int i = Integer.parseInt(st.nextToken());
+            int j = Integer.parseInt(st.nextToken());
+            int k = Integer.parseInt(st.nextToken());
+            for (int m = i; m <= j; m++) {
+                basket[m-1] = k;
+            }
+        }
+
+        for (int i : basket) {
+            bw.write(i + " ");
+        }
+
+        bw.close();
+    }
+}
