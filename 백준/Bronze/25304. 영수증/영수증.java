82de361942f17495f9c25bb46159ed566f4c00bc
Hyuk <ish0411@naver.com>
2023-11-14 16:44:34 +0900

    [Bronze IV] Title: 영수증, Time: 124 ms, Memory: 14144 KB -BaekjoonHub

---
+++ 백준/Bronze/25304. 영수증/영수증.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int X = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
+
+        int sum = 0;
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            sum += a * b;
+        }
+
+        if (X == sum) {
+            bw.write("Yes");
+        } else {
+            bw.write("No");
+        }
+
+        bw.close();
+    }
+}
