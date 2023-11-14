9aec6cc2f2fa3ac130d627aeb5093dabc0915cbd
Hyuk <ish0411@naver.com>
2023-11-14 16:59:05 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 128 ms, Memory: 14252 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -0,0 +1,19 @@
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i < N + 1; i++) {
+            for (int j = 0; j < i; j++) {
+                bw.write("*");
+            }
+            bw.newLine();
+        }
+
+        bw.close();
+    }
+}
