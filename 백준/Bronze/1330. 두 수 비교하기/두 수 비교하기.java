ccf50192d32be7414009a0c2665e953899dda87c
Hyuk <ish0411@naver.com>
2023-11-14 16:02:50 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 128 ms, Memory: 14256 KB -BaekjoonHub

---
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+
+        if (A > B) {
+            bw.write(">");
+        } else if (A < B) {
+            bw.write("<");
+        } else {
+            bw.write("==");
+        }
+        
+        bw.close();
+    }
+}
