c95208852e2244efbacb5e946ba873cfb13428ec
Hyuk <ish0411@naver.com>
2023-11-14 16:38:55 +0900

    [Bronze V] Title: 합, Time: 124 ms, Memory: 14284 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+
+        bw.write((int) n * (n + 1) / 2 + "");
+        bw.close();
+    }
+}
