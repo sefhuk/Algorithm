4ef1601946418ea15c9ed6334f408606fcde204b
Hyuk <ish0411@naver.com>
2023-11-14 15:12:39 +0900

    [Bronze V] Title: ??!, Time: 132 ms, Memory: 14276 KB -BaekjoonHub

--- 백준/Bronze/10926. ？？！/？？！.java
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -2,11 +2,11 @@ import java.io.*;
 
 class Main {
     public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        BufferedReader br = new BufferedReader(new InputStreamReader((System.in)));
 
-        String input = br.readLine();
-        bw.write(input + "??!");
+        String input = br.readLine() + "??!";
+        bw.write(input);
         bw.close();
     }
 }
