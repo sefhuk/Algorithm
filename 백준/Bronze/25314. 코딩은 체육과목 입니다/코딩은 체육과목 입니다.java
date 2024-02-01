a4c131283dd8ac5695c5f38fdc68e239989a5fca
Hyuk <ish0411@naver.com>
2024-02-01 10:31:44 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 124 ms, Memory: 14340 KB -BaekjoonHub

--- 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.java
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.java
@@ -1,18 +1,15 @@
+import java.util.*;
 import java.io.*;
 
 class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        int bytes = Integer.parseInt(br.readLine()) / 4;
-
-        for (int i = 0; i < bytes; i++) {
-            bw.write("long ");
+        int n = Integer.parseInt(br.readLine());
+        
+        for(int i = 0; i < n / 4; i++) {
+            System.out.print("long ");
         }
 
-        bw.write("int");
-
-        bw.close();
+				System.out.println("int");
     }
 }
