2d71e681735692696d392d1ad654a8eb7e5bb1f1
Hyuk <ish0411@naver.com>
2024-02-01 11:17:55 +0900

    [Bronze V] Title: 문자와 문자열, Time: 120 ms, Memory: 14200 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        int i = Integer.parseInt(br.readLine());
+        System.out.println(s.charAt(i - 1));
+    }
+}
