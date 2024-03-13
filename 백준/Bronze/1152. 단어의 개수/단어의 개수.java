a48340f5e88a3ab395845b227b525b46696b2ec3
Hyuk <ish0411@naver.com>
2024-03-13 22:33:10 +0900

    [Bronze II] Title: 단어의 개수, Time: 248 ms, Memory: 20044 KB -BaekjoonHub

---
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		bw.write(st.countTokens() + "");
+		bw.close();
+	}
+}
